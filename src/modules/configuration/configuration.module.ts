acad2d434b8b9a834aa4dcd3dfeafbfe826806b6
Grayson Cunha <graysonsilva93@gmail.com>
2023-07-11 23:44:56 -0300

    feat: configure database with enviroment variables

--- src/modules/configuration/configuration.module.ts
+++ src/modules/configuration/configuration.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `${process.env.DATABASE_URL_BASE}${process.env.DATABASE_NAME}`,
+    ),
+  ],
 })
 export class ConfigurationModule {}
